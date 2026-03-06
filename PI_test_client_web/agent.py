6e881f30533214e712a27341f823df095075869e
mar_hiro <ma.hiro.yogi.carvalho@gmail.com>
2026-03-06 10:45:47 -0300

    updating

--- PI_test_client_web/agent.py
+++ PI_test_client_web/agent.py
@@ -1,33 +1,59 @@
 import psutil
-import requests
+import csv
+import time
+from datetime import date, timedelta, datetime
 
-import GPUtil
-
-from datetime import datetime, timezone
-
-SERVER_URL = "http://127.0.0.1:8000/api/metrics"
-INTERVAL_SECONDS = 2
+CSV_FILE = "system_metrics.csv"
+DeltaTime = timedelta(days=0, seconds=30)
+TEMPO = 2
 
 def collect_metrics():
+
     cpu = psutil.cpu_percent(interval=0.2)
     mem = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
-
+    data = datetime.now().isoformat()
     return {
-        "cpu_percent": float(cpu),
-        "mem_percent": float(mem),
-        "disk_percent": float(disk),
+        "timestamp": data,
+        "cpu_percent": cpu,
+        "mem_percent": mem,
+        "disk_percent": disk,
     }
 
-def main():
-    while True:
-        payload = collect_metrics()
-        try:
-            r = requests.post(SERVER_URL, json=payload, timeout=3)
-            r.raise_for_status()
-        except Exception as e:
-            print("Send failed:", e)
-        time.sleep(INTERVAL_SECONDS)
+def cleaner():
+    payload = collect_metrics()
+    c = open(CSV_FILE, mode='w', newline='')
+    CSV_cleaner = csv.DictWriter(c, fieldnames=payload.keys(), delimiter=';')
+    CSV_cleaner.writeheader()
 
+
+
+def main():
+    while True: 
+        payload = collect_metrics()
+        current_timestamp = datetime.now()
+
+
+        f = open(CSV_FILE, 'a', newline='')
+        writer = csv.DictWriter(f, fieldnames=payload.keys(), delimiter=';')
+        writer.writerow(payload)
+            
+
+        r = open(CSV_FILE, 'r', newline='')
+        reader = csv.DictReader(r, delimiter=';')
+        row = next(reader, None)
+        if row:
+
+            diference = current_timestamp - datetime.fromisoformat(row['timestamp'])
+
+            print(diference)
+
+            if(diference >= DeltaTime):
+
+                cleaner()
+                
+        time.sleep(TEMPO)
 if __name__ == "__main__":
+    cleaner()
     main()
+
